ccd61e257be5fbd77d69d8adb1aeb35b91270f02
dan1delon <143418624+dan1delon@users.noreply.github.com>
2025-04-23 18:50:28 +0300

    done dashboard

--- src/components/RecommendedPage/Dashboard/Dashboard.tsx
+++ src/components/RecommendedPage/Dashboard/Dashboard.tsx
@@ -1,12 +1,24 @@
+import { useMediaQuery } from '@mui/material';
 import Filters from '../Filters/Filters';
 import Walkthrough from '../Walkthrough/Walkthrough';
 import css from './Dashboard.module.css';
 
 const Dashboard = () => {
+  const isPc = useMediaQuery('(min-width: 1280px)');
+
   return (
     <div className={css.container}>
       <Filters />
       <Walkthrough />
+      {isPc && (
+        <div className={css.quoteWrapper}>
+          <img src="/img/books.png" alt="books" className={css.quoteIcon} />
+          <p className={css.quoteText}>
+            "Books are <span className={css.highlight}>windows</span> to the
+            world, and reading is a journey into the unknown."
+          </p>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/RecommendedPage/Walkthrough/Walkthrough.tsx
+++ src/components/RecommendedPage/Walkthrough/Walkthrough.tsx
@@ -1,7 +1,41 @@
+import { NavLink } from 'react-router-dom';
 import css from './Walkthrough.module.css';
+import Icon from '../../../shared/Icon/Icon';
 
 const Walkthrough = () => {
-  return <div className={css.container}>Walkthrough</div>;
+  return (
+    <div className={css.container}>
+      <h2 className={css.title}>Start your workout</h2>
+      <ul className={css.list}>
+        <li className={css.listItem}>
+          <div className={css.listItemNumber}>1</div>
+          <p className={css.listItemText}>
+            <span className={css.listItemHighlight}>
+              Create a personal library:
+            </span>{' '}
+            add the books you intend to read <br /> to it.
+          </p>
+        </li>
+        <li className={css.listItem}>
+          <div className={css.listItemNumber}>2</div>
+          <p className={css.listItemText}>
+            <span className={css.listItemHighlight}>
+              Create your first workout:
+            </span>{' '}
+            define a goal, choose a period, start training.
+          </p>
+        </li>
+      </ul>
+      <div className={css.btnWrapper}>
+        <NavLink to="/library" className={css.btnLink}>
+          My library
+        </NavLink>
+        <NavLink to="/library" className={css.btnLinkArrow}>
+          <Icon iconId="icon-arrow" className={css.iconArrow} />
+        </NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default Walkthrough;
